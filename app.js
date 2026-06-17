373595794dc0a7b80cf75821a107ef10ef1b7849
degrassed-mastermind <degrassed@gmail.com>
2026-06-17 04:29:18 -0400

    Lighten active bottom nav tab

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const SUPABASE_CONFIG = {
 };
 
 const PLATFORM_REVIEWER_EMAIL = "degrassed@gmail.com";
-const APP_VERSION = "v98";
+const APP_VERSION = "v99";
 
 const PERIOD_FORMATS = {
   quarters: {
