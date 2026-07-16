e89460cb27f0012ac93178fb167a4c574151abe0
degrassed-mastermind <degrassed@gmail.com>
2026-07-15 22:06:43 -0400

    Expand LaxHornet marketing site pages

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const SUPABASE_CONFIG = {
 };
 
 const PLATFORM_REVIEWER_EMAIL = "degrassed@gmail.com";
-const APP_VERSION = "v273";
+const APP_VERSION = "v274";
 
 const PERIOD_FORMATS = {
   quarters: {
