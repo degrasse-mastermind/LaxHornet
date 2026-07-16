43905c191aad30c6cd6a32ab235610513bcc3ec2
degrassed-mastermind <degrassed@gmail.com>
2026-07-15 21:52:11 -0400

    Create club leadership landing page

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const SUPABASE_CONFIG = {
 };
 
 const PLATFORM_REVIEWER_EMAIL = "degrassed@gmail.com";
-const APP_VERSION = "v272";
+const APP_VERSION = "v273";
 
 const PERIOD_FORMATS = {
   quarters: {
