adc7f19897fa803ff954d5ef91e81d0e1a1f8c67
degrassed-mastermind <degrassed@gmail.com>
2026-07-23 05:48:27 -0400

    Add game scope capability handshake

--- runtime-config.js
+++ runtime-config.js
@@ -3,5 +3,6 @@ window.LAXHORNET_RUNTIME_CONFIG = Object.freeze({
   publicLiveShareRpc: true,
   liveShareTokenRpc: true,
   exportAuditRpc: true,
+  minimumSchemaCapability: 1,
 });
 window.LAXHORNET_SCRIPT_ORDER = [...(window.LAXHORNET_SCRIPT_ORDER || []), "runtime-config"];
---
+++ supabase/rollback/20260723040000_event_pipeline_capabilities_rollback.sql
@@ -0,0 +1,5 @@
+begin;
+
+drop function if exists public.lh_release_capabilities();
+
+commit;
